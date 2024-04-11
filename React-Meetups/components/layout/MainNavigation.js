056c0a05f5e6fc88bc5f1fa957bf85b2599d46f1
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-11 14:46:25 +0330

    Update All Meetups & Add New Meetup UI

--- React-Meetups/components/layout/MainNavigation.js
+++ React-Meetups/components/layout/MainNavigation.js
@@ -3,16 +3,27 @@ import Link from 'next/link';
 export default function MainNavigation() {
   return (
     <header className="nav-header">
-      <div className="logo"> React Meetups </div>
+      <div className="logo">
+        <img
+          src="https://skillicons.dev/icons?i=nextjs"
+          height="35"
+          alt="nextjs logo"
+        />
+        <h1>React & Next Meetups</h1>
+      </div>
 
       <nav>
         <ul>
           <li>
-            <Link href="/"> All Meetups </Link>
+            <Link href="/" >
+              All Meetups
+            </Link>
           </li>
 
           <li>
-            <Link href="/newMeetup"> Add New Meetup </Link>
+            <Link href="/newMeetup" >
+              Add New Meetup
+            </Link>
           </li>
         </ul>
       </nav>
