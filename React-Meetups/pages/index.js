25e762b21306ca008f69eea6944986bd191cfc6f
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-13 14:25:20 +0330

    More on Static Site Generation (SSG) | revalidate

--- React-Meetups/pages/index.js
+++ React-Meetups/pages/index.js
@@ -38,12 +38,13 @@ export async function getStaticProps() {
     props: {
       meetups: DUMMY_MEETUPS,
     },
+
+    revalidate: 10, // 10 seconds | 3600 1H
   };
 }
 
 /* NextJS Rendering
 - we might have SEO Problems if we use useEffect() to fetch-data
-
 * Does not wait for the data to be fetched to then return the fully pre-rendered page, but it returns the result of the first component rendered cycle and that might be a pretty empty page
 
 BUT Nextjs has 2 solution for pre-rendering the content-data
@@ -51,7 +52,6 @@ BUT Nextjs has 2 solution for pre-rendering the content-data
     a page component is pre-rendered when you build your application, when you build the Next project, When You build it for the production(npm run build)
 
   - Server-side Rendering (SSR)
-
 */
 
 /* getStaticProps()
@@ -68,5 +68,8 @@ BUT Nextjs has 2 solution for pre-rendering the content-data
 
 - inside this getStaticProps() you could access a file system or securely connect to a database
 * because any code you write in here will never end up on the client-side and it will never execute on the client-side, simply because this code is executed during the "build-process"
+*/
 
+/* revalidate
+- this page will not just build during the build process, but it also be generated every couple of seconds on the server, at least if there are requests
 */
