1ceb8f49e22942a9b0966227d078a8d1caaba856
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-13 12:51:15 +0330

    Data Fetching with Pre-rendering | Static Site Generation (SSG)

--- React-Meetups/pages/index.js
+++ React-Meetups/pages/index.js
@@ -21,10 +21,52 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-export default function HomePage() {
+export default function HomePage({ meetups }) {
   return (
     <>
-      <MeetupList meetups={DUMMY_MEETUPS} />
+      <MeetupList meetups={meetups} />
     </>
   );
 }
+
+// if you need to add data-fetching to a page-component | fn = reserves-name
+export async function getStaticProps() {
+  // fetch data from an API
+
+  return {
+    //this props will be the props-obj you receive in your page-component-fn
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
+
+/* NextJS Rendering
+- we might have SEO Problems if we use useEffect() to fetch-data
+
+* Does not wait for the data to be fetched to then return the fully pre-rendered page, but it returns the result of the first component rendered cycle and that might be a pretty empty page
+
+BUT Nextjs has 2 solution for pre-rendering the content-data
+  - Static Site Generation (SSG):
+    a page component is pre-rendered when you build your application, when you build the Next project, When You build it for the production(npm run build)
+
+  - Server-side Rendering (SSR)
+
+*/
+
+/* getStaticProps()
+?- Nextjs first of all, call getStaticProps() before it calls the component-function
+
+- its job is to prepare the props for this page
+- and this props could then contain the data this page needs
+
+?- getStaticProps() is allowed to be "asynchronous"
+- you can return a promise there
+- nextjs will wait for this promise to resolve
+- it waits until your data is loaded
+- then you return the props for this component-fn
+
+- inside this getStaticProps() you could access a file system or securely connect to a database
+* because any code you write in here will never end up on the client-side and it will never execute on the client-side, simply because this code is executed during the "build-process"
+
+*/
