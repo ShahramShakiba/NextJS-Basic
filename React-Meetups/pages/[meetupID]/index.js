b1166c1a0bf073c6c50434924037d981ad1946f3
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-14 12:15:40 +0330

    Preparing Paths with getStaticPaths | Working With Fallback Pages

--- React-Meetups/pages/[meetupID]/index.js
+++ React-Meetups/pages/[meetupID]/index.js
@@ -1,9 +1,9 @@
 import MeetupDetail from '@/components/meetups/MeetupDetail';
 
-export default function MeetupDetails({ meetupData }) {
+export default function MeetupDetails() {
   return (
     <MeetupDetail
-      image={meetupData.image}
+      image="https://resources.sansan.com/hubfs/Imported_Blog_Media/GettyImages-530685779-Article-9.jpg"
       title="First Meetup"
       address="London"
       description="This is first meetup."
@@ -11,10 +11,32 @@ export default function MeetupDetails({ meetupData }) {
   );
 }
 
-export async function getStaticProps(context) {
-  // fetch data for a single meetup
+// getStaticPaths() is a function you need to export if you have a dynamic-page and you're using getStaticProps()
+export async function getStaticPaths() {
+  return {
+    fallback: false,
 
-  // identifier between the square-brackets will be properties and the value will be the actual value encoded in the URL
+    // array of all the possible dynamic paths
+    paths: [
+      {
+        params: {
+          meetupID: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupID: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+// Page pre-generated during the build-process
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+
+  //identifier between the square-brackets will be properties and the value will be the actual value encoded in the URL
   const meetupID = context.params.meetupID;
 
   return {
@@ -42,4 +64,16 @@ export async function getStaticProps(context) {
 ?- you can use brackets [] in the page filename to denote dynamic segments.
 
 - For example, if you create a file named [id].js inside the pages directory, you can access the dynamic parameter id in your page component using useRouter from next/router.
+*/
+
+/* fallback
+
+- fallback it allows you to pre-generated some of your pages for a specific meetupID values
+
+?- false:
+   your path contains all supported meetupID values, if user enter anything that's not supported here, throw a 404 error - new paths will result in a 404 page
+
+?- true:
+   next.js would try to generate a page dynamically on the server for the incoming request and if it fails, it will serve the fallback page
+
 */
