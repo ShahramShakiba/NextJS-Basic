dc3707e80206fb07a6e9954a8a89babd75dcf6fc
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-14 14:59:14 +0330

    Working with MongoDB

--- React-Meetups/pages/api/newMeetup.js
+++ React-Meetups/pages/api/newMeetup.js
@@ -1,14 +1,48 @@
-// Next.js will pick up any JavaScript files stored in there(in api-folder) and turn those files into API routes, so into endpoints that can be targeted by request that should receive JSON and return JSON
+/* Next.js will pick up any JavaScript files stored in there(in api-folder) and turn those files into API routes, so into endpoints that can be targeted by request that should receive JSON and return JSON
 
-// path:   /api/newMeetup
+- The code defined in here will never end up on the client-side
+- this is secure place to store "credentials"
+- path:   /api/newMeetup
+*/
+
+import { MongoClient } from 'mongodb';
 
 // req = contains data about the incoming request
 // res = sending back a response
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body; // data of incoming request
 
-    // 4 expected field on the incoming req 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      'mongodb+srv://shahram:96C5ScLzZs4ghP7Z@cluster-sh.rgaxnx4.mongodb.net/?retryWrites=true&w=majority&appName=react-next-meetups'
+    );
+    const db = client.db(); // get hold of the database-> react-next-meetups
+
+    const meetupsCollection = db.collection('meetups');
+
+    // inserting one new document(is just ana obj) into this collection
+    const result = meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    //send back a response | 201 = sth inserted successfully
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
+
+/* collection
+  collections full of documents
+  document
+    a single entry in a collection
+
+- MongoDB is a NoSQL-database
+- it is not relational database
+- it is a document-based database
+
+- collections would be kind of your table in SQL-database
+- document would be your entries in those tables
+
+-> you have collections which hold multiple documents and a single meetup would be a single document
+
+*/
